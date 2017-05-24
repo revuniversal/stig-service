7eb0988844ab369febc0743b13c18e484102e40e
JP Dillingham <jpdillingham@gmail.com>
2017-05-23 22:15:22 -0500

    Converted app to typescript and added site scrape

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var axios_1 = require("axios");
 console.log('hello world!');
+axios_1["default"].get('http://iase.disa.mil/stigs/Pages/a-z.aspx')
+    .then(function (response) {
+    console.log(response);
+});
