acd0771272e1b570b096e4dec070c2e818f5c5fb
Rev Herr <revuniversal@gmail.com>
2017-06-07 16:20:17 -0500

    feat: fetch link text/url pairs

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
-import axios from "axios";
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import { map } from 'lodash';
 
-console.log('hello world!');
+type StigLink = {
+  name: string;
+  url: string;
+};
 
-axios.get('http://iase.disa.mil/stigs/Pages/a-z.aspx')
-    .then((response) => {
-        console.log(response);
-    })
+axios.get('http://iase.disa.mil/stigs/Pages/a-z.aspx').then(res => {
+  const html: string = res.data;
+  const $ = cheerio.load(html);
+  const links = $('a');
+  const stigLinks: StigLink[] = [];
+
+  for (let i = 0; i < links.length; i++) {
+    let el = $(links[i]);
+    let link = {
+      name: el.text(),
+      url: el.attr('href')
+    };
+
+    if (link.url && link.url.indexOf('.zip') > -1) {
+      stigLinks.push(link);
+    }
+  }
+
+  console.log('stigs: ' + stigLinks.length);
+});
